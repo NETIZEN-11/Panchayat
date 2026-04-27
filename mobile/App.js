bb1e3606da2a58cad2ab32e29ed19d874d097651
Nitesh <nitesh@example.com>
2026-04-27 21:06:21 +0530

    chore(mobile): update dependencies and root App component

--- mobile/App.js
+++ mobile/App.js
@@ -3,9 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet } from 'react-native';
 import { AuthProvider } from './src/context/AuthContext';
 import { LanguageProvider } from './src/context/LanguageContext';
+import { ThemeProvider } from './src/context/ThemeContext';
 import { SchemeProvider } from './src/context/SchemeContext';
 import { ComplaintProvider } from './src/context/ComplaintContext';
-import AppNavigator from './src/navigation/AppNavigator';
+import { SocketProvider } from './src/context/SocketContext';
+import AppNavigator from './src/navigation/Navigation';
 
 export default function App() {
   return (
@@ -13,11 +15,15 @@ export default function App() {
       <StatusBar style="light" />
       <AuthProvider>
         <LanguageProvider>
-          <SchemeProvider>
-            <ComplaintProvider>
-              <AppNavigator />
-            </ComplaintProvider>
-          </SchemeProvider>
+          <ThemeProvider>
+            <SchemeProvider>
+              <ComplaintProvider>
+                <SocketProvider>
+                  <AppNavigator />
+                </SocketProvider>
+              </ComplaintProvider>
+            </SchemeProvider>
+          </ThemeProvider>
         </LanguageProvider>
       </AuthProvider>
     </View>
